ebf7a279126f30d75fb686bc13e27c2327b8d625
Hladkov Danylo <danylo.hladkov@nure.ua>
2020-05-20 17:27:59 +0300

    Improved token handling using Angular JwtHelperService

--- SWMedia-SPA/src/app/_services/auth.service.ts
+++ SWMedia-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient ) { }
 
@@ -17,13 +20,20 @@ login(model: any) {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
-    )
+    );
 }
 
 register(model:any) {
   return this.http.post(this.baseUrl + 'registerUser', model);
+}
+
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
